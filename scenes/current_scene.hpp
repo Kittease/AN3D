8e2de0d8e77aff2a221ba6d59bccb7ec902f1a8f
Carl-Adrien Mercey <camercey@gmail.com>
2021-11-04 18:03:30 +0100

    pushed existing project

---
+++ scenes/current_scene.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+// Indicate here which exercise should be currently considered using its define keyword
+#define SCENE_BOIDS // replace this keyword to change the scene
+
+// Help: The keywords associated to other scenes are listed below
+//
+// #define SCENE_DEFAULT_3D_GRAPHICS
+// #define SCENE_INTERPOLATION_POSITION
+// #define SCENE_ARTICULATED_HIERARCHY
+// #define SCENE_SPHERE_COLLISION
+// #define SCENE_MASS_SPRING_1D
+// #define SCENE_CLOTH
+// #define SCENE_SKINNING
