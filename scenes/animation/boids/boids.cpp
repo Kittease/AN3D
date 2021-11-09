094cb95e2fcd8df69508a1aa473e0108b970fe62
lukas.rabier <lukas.rabier@epita.fr>
2021-11-09 17:09:13 +0100

    [FOV] fov implementation, appearing with red color for first mate

--- scenes/animation/boids/boids.cpp
+++ scenes/animation/boids/boids.cpp
@@ -7,7 +7,12 @@
 
 using namespace vcl;
 
-static void set_gui(timer_basic &timer);
+static void set_gui(timer_basic &timer, float &angle);
+
+float computeAngle(const vcl::vec3 &a, const vcl::vec3 &b)
+{
+    return acosf(dot(a, b) / (norm(a) + norm(b))) * 180 / M_PI;
+}
 
 void scene_model::setup_data(std::map<std::string, GLuint> &shaders,
                              scene_structure &, gui_structure &gui)
@@ -25,13 +30,14 @@ void scene_model::setup_data(std::map<std::string, GLuint> &shaders,
 
     gui.show_frame_camera = false;
 
-    flock.resize(n_mates);
+    mates.reserve(n_mates);
     random_real_generator init_gen{ -1, 1 };
     for (int i = 0; i < n_mates; i++)
     {
         vcl::vec3 dir{ init_gen(), init_gen(), init_gen() };
         dir = vcl::normalize(dir);
-        flock[i] = mate{ { init_gen(), init_gen(), init_gen() }, dir, 0.25 };
+        mates.push_back(std::make_shared<mate>(
+            mate{ { init_gen(), init_gen(), init_gen() }, dir, 0.25, 0.5 }));
     }
 
     mate_mesh =
@@ -39,43 +45,49 @@ void scene_model::setup_data(std::map<std::string, GLuint> &shaders,
     mate_mesh.shader = shaders["mesh"];
 }
 
-void scene_model::frame_draw(std::map<std::string, GLuint> &shaders,
-                             scene_structure &scene, gui_structure &)
+void scene_model::update_flock()
 {
     float dt = 0.02f * timer.scale;
-    timer.update();
-    set_gui(timer);
-
-    draw(borders, scene.camera, shaders["curve"]);
-
-    for (int i = 0; i < n_mates; i++)
+    for (auto &mate : mates)
     {
-        vcl::vec3 dir = flock[i].dir;
-        vcl::vec3 position = flock[i].pos;
-
-        vcl::vec3 old_pos = flock[i].pos;
+        mate->drawn = false;
+        vcl::vec3 old_pos = mate->pos;
         vcl::vec3 random_dir_variation{ var_gen(), var_gen(), var_gen() };
-        vcl::vec3 new_dir = vcl::normalize(dir + random_dir_variation);
-        vcl::vec3 new_pos = position + dt * (flock[i].speed * new_dir);
 
-        flock[i].pos = new_pos;
-        flock[i].dir = new_dir;
-
-        mate_mesh.uniform.transform.translation = new_pos;
-        mate_mesh.uniform.transform.rotation = rotation_between_vector_mat3(
-            { 1, 0, 0 }, normalize(new_pos - old_pos));
-
-        mate_mesh.uniform.color = { 0.2, 0.8, 1 };
-        draw(mate_mesh, scene.camera);
+        vcl::vec3 new_dir = vcl::normalize(mate->dir + random_dir_variation);
+        vcl::vec3 new_pos = old_pos + dt * (mate->speed * new_dir);
+        mate->dir = new_dir;
+        mate->pos = new_pos;
+        mate->findVisibleMates(mates, mate_view_angle);
     }
 }
 
-static void set_gui(timer_basic &timer)
+void scene_model::frame_draw(std::map<std::string, GLuint> &shaders,
+                             scene_structure &scene, gui_structure &)
+{
+    timer.update();
+    set_gui(timer, mate_view_angle);
+
+    update_flock();
+    mates[0]->draw_mate(mate_mesh, scene.camera, { 0.2, 0.3, 1 });
+    for (const auto &mate : mates[0]->visibleMates)
+        mate->draw_mate(mate_mesh, scene.camera, { 1., 0.2, 0.3 });
+    for (const auto &mate : mates)
+        mate->draw_mate(mate_mesh, scene.camera, { 0.2, 0.8, 1 });
+    draw(borders, scene.camera, shaders["curve"]);
+}
+
+static void set_gui(timer_basic &timer, float &angle)
 {
     float scale_min = 0.05f;
     float scale_max = 2.0f;
     ImGui::SliderScalar("Time scale", ImGuiDataType_Float, &timer.scale,
                         &scale_min, &scale_max, "%.2f s");
+
+    float angle_min = 60;
+    float angle_max = 360;
+    ImGui::SliderScalar("Mate FOV", ImGuiDataType_Float, &angle, &angle_min,
+                        &angle_max, "%.2f s");
 
     if (ImGui::Button("Stop"))
         timer.stop();
--- scenes/animation/boids/boids.hpp
+++ scenes/animation/boids/boids.hpp
@@ -1,15 +1,63 @@
 #pragma once
 
+#include <memory>
+
 #include "main/scene_base/base.hpp"
 
 #ifdef SCENE_BOIDS
-#    define RANDOM_VARIATION 0.5f
+#    define RANDOM_VARIATION 0.3f
+
+struct mate;
+using flock = std::vector<std::shared_ptr<mate>>;
+
+float computeAngle(const vcl::vec3 &a, const vcl::vec3 &b);
 
 struct mate
 {
     vcl::vec3 pos;
     vcl::vec3 dir;
     float speed;
+    float radius;
+    bool drawn = false;
+    flock visibleMates;
+
+    mate(vcl::vec3 pos, vcl::vec3 dir, float speed, float radius)
+        : pos{ pos }
+        , dir{ dir }
+        , speed{ speed }
+        , radius{ radius }
+    {}
+
+    void draw_mate(vcl::mesh_drawable &mate_mesh, vcl::camera_scene &camera,
+                   vcl::vec3 color)
+    {
+        if (!drawn)
+        {
+            mate_mesh.uniform.transform.translation = pos;
+            mate_mesh.uniform.transform.rotation =
+                rotation_between_vector_mat3({ 1, 0, 0 }, dir);
+
+            mate_mesh.uniform.color = color;
+            draw(mate_mesh, camera);
+            drawn = true;
+        }
+    }
+
+    void findVisibleMates(const flock &mates, float angle)
+    {
+        visibleMates.clear();
+        for (const auto &mate : mates)
+        {
+            if (mate.get() == this)
+                continue;
+            auto curToMate = mate->pos - pos;
+            if (vcl::norm(curToMate) < radius
+                && computeAngle(dir, curToMate) < angle / 2)
+            {
+                visibleMates.emplace_back(mate);
+            }
+        }
+    }
 };
 
 class random_real_generator
@@ -38,9 +86,11 @@ struct scene_model : scene_base
                     scene_structure &scene, gui_structure &gui);
     void frame_draw(std::map<std::string, GLuint> &shaders,
                     scene_structure &scene, gui_structure &gui);
+    void update_flock();
 
-    int n_mates = 100;
-    std::vector<mate> flock;
+    int n_mates = 800;
+    float mate_view_angle = 180;
+    flock mates;
     vcl::mesh_drawable mate_mesh;
 
     vcl::segments_drawable borders;
