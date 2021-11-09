cf4b7cc0fe969ef4cb77585bd66b5eb829402708
lukas.rabier <lukas.rabier@epita.fr>
2021-11-09 12:27:02 +0100

    [MOVEMENT] implemented random movement with static speed

--- scenes/animation/boids/boids.cpp
+++ scenes/animation/boids/boids.cpp
@@ -1,4 +1,5 @@
 #include "boids.hpp"
+
 #include "../../../vcl/math/transformation/transformation.hpp"
 #include "../../../vcl/wrapper/perlin/perlin.hpp"
 
@@ -6,65 +7,79 @@
 
 using namespace vcl;
 
+static void set_gui(timer_basic &timer);
 
-static void set_gui(timer_basic& timer);
-
-void scene_model::setup_data(std::map<std::string, GLuint>& shaders, scene_structure&, gui_structure& gui) {
-    std::vector<vec3> borders_segments = {{-1,-1,-1},{1,-1,-1}, {1,-1,-1},{1,1,-1}, {1,1,-1},{-1,1,-1}, {-1,1,-1},{-1,-1,-1},
-                                          {-1,-1,1} ,{1,-1,1},  {1,-1,1}, {1,1,1},  {1,1,1}, {-1,1,1},  {-1,1,1}, {-1,-1,1},
-                                          {-1,-1,-1},{-1,-1,1}, {1,-1,-1},{1,-1,1}, {1,1,-1},{1,1,1},   {-1,1,-1},{-1,1,1}};
+void scene_model::setup_data(std::map<std::string, GLuint> &shaders,
+                             scene_structure &, gui_structure &gui)
+{
+    std::vector<vec3> borders_segments = {
+        { -1, -1, -1 }, { 1, -1, -1 }, { 1, -1, -1 }, { 1, 1, -1 },
+        { 1, 1, -1 },   { -1, 1, -1 }, { -1, 1, -1 }, { -1, -1, -1 },
+        { -1, -1, 1 },  { 1, -1, 1 },  { 1, -1, 1 },  { 1, 1, 1 },
+        { 1, 1, 1 },    { -1, 1, 1 },  { -1, 1, 1 },  { -1, -1, 1 },
+        { -1, -1, -1 }, { -1, -1, 1 }, { 1, -1, -1 }, { 1, -1, 1 },
+        { 1, 1, -1 },   { 1, 1, 1 },   { -1, 1, -1 }, { -1, 1, 1 }
+    };
     borders = borders_segments;
-    borders.uniform.color = {0,0,0};
+    borders.uniform.color = { 0, 0, 0 };
 
     gui.show_frame_camera = false;
 
-
     flock.resize(n_mates);
     for (int i = 0; i < n_mates; i++)
-        flock[i] = mate{ { 0, 0, 0 }, { 0.25, 0.25, 0.25 } };
+    {
+        vcl::vec3 dir{ 0.25, 0.25, 0. };
+        dir = vcl::normalize(dir);
+        flock[i] = mate{ { 0, 0, 0 }, dir, 0.25 };
+    }
 
-
-    mate_mesh = mesh_drawable(mesh_primitive_cone(0.02, { 0, 0, 0 }, { 0.06, 0, 0 }));
+    mate_mesh =
+        mesh_drawable(mesh_primitive_cone(0.02, { 0, 0, 0 }, { 0.06, 0, 0 }));
     mate_mesh.shader = shaders["mesh"];
 }
 
-void scene_model::frame_draw(std::map<std::string,GLuint>& shaders, scene_structure& scene, gui_structure& ) {
+void scene_model::frame_draw(std::map<std::string, GLuint> &shaders,
+                             scene_structure &scene, gui_structure &)
+{
+    float dt = 0.02f * timer.scale;
     timer.update();
     set_gui(timer);
-    
-    float t = timer.t;
 
     draw(borders, scene.camera, shaders["curve"]);
 
-
-
-    for (int i = 0; i < n_mates; i++) {
-        vcl::vec3 speed = flock[i].speed;
-
-        float noiseX = perlin(i + t * speed.x, 0.0f, 0.0f) * 2 - 1.4;
-        float noiseY = perlin(0.0f, i + t * speed.y, 0.0f) * 2 - 1.4;
-        float noiseZ = perlin(0.0f, 0.0f, i + t * speed.z) * 2 - 1.4;
+    for (int i = 0; i < n_mates; i++)
+    {
+        vcl::vec3 dir = flock[i].dir;
+        vcl::vec3 position = flock[i].pos;
 
         vcl::vec3 old_pos = flock[i].pos;
-        vcl::vec3 new_pos = { noiseX, noiseY, noiseZ };
+        vcl::vec3 random_dir_variation{ gen(), gen(), gen() };
+        vcl::vec3 new_dir = vcl::normalize(dir + random_dir_variation);
+        vcl::vec3 new_pos = position + dt * (flock[i].speed * new_dir);
 
         flock[i].pos = new_pos;
+        flock[i].dir = new_dir;
 
         mate_mesh.uniform.transform.translation = new_pos;
-        mate_mesh.uniform.transform.rotation = rotation_between_vector_mat3({ 1, 0, 0 }, normalize(new_pos - old_pos));
+        mate_mesh.uniform.transform.rotation = rotation_between_vector_mat3(
+            { 1, 0, 0 }, normalize(new_pos - old_pos));
 
         mate_mesh.uniform.color = { 0.2, 0.8, 1 };
         draw(mate_mesh, scene.camera);
     }
 }
 
-static void set_gui(timer_basic& timer) {
+static void set_gui(timer_basic &timer)
+{
     float scale_min = 0.05f;
     float scale_max = 2.0f;
-    ImGui::SliderScalar("Time scale", ImGuiDataType_Float, &timer.scale, &scale_min, &scale_max, "%.2f s");
+    ImGui::SliderScalar("Time scale", ImGuiDataType_Float, &timer.scale,
+                        &scale_min, &scale_max, "%.2f s");
 
-    if (ImGui::Button("Stop"))  timer.stop();
-    if (ImGui::Button("Start")) timer.start();
+    if (ImGui::Button("Stop"))
+        timer.stop();
+    if (ImGui::Button("Start"))
+        timer.start();
 }
 
 #endif
--- scenes/animation/boids/boids.hpp
+++ scenes/animation/boids/boids.hpp
@@ -3,15 +3,41 @@
 #include "main/scene_base/base.hpp"
 
 #ifdef SCENE_BOIDS
+#    define RANDOM_VARIATION 0.5f
 
-struct mate {
+struct mate
+{
     vcl::vec3 pos;
-    vcl::vec3 speed;
+    vcl::vec3 dir;
+    float speed;
 };
 
-struct scene_model : scene_base {
-    void setup_data(std::map<std::string,GLuint>& shaders, scene_structure& scene, gui_structure& gui);
-    void frame_draw(std::map<std::string,GLuint>& shaders, scene_structure& scene, gui_structure& gui);
+class random_real_generator
+{
+public:
+    random_real_generator(float a, float b)
+    {
+        gen = std::mt19937(rd());
+        dis = std::uniform_real_distribution<float>(a, b);
+    }
+
+    float operator()()
+    {
+        return dis(gen);
+    }
+
+protected:
+    std::random_device rd;
+    std::mt19937 gen;
+    std::uniform_real_distribution<float> dis;
+};
+
+struct scene_model : scene_base
+{
+    void setup_data(std::map<std::string, GLuint> &shaders,
+                    scene_structure &scene, gui_structure &gui);
+    void frame_draw(std::map<std::string, GLuint> &shaders,
+                    scene_structure &scene, gui_structure &gui);
 
     int n_mates = 100;
     std::vector<mate> flock;
@@ -20,6 +46,7 @@ struct scene_model : scene_base {
     vcl::segments_drawable borders;
 
     vcl::timer_event timer;
+    random_real_generator gen{ -RANDOM_VARIATION, RANDOM_VARIATION };
 };
 
 #endif
